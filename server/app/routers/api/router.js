aad703411563b25b8c30c654e39fa84fad857101
Thien Nguyen <thien.ngu25@gmail.com>
2024-05-21 17:28:35 +0200

    modify router.js

--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -5,17 +5,17 @@ const router = express.Router();
 /* ************************************************************************* */
 const { sayWelcome } = require("../../controllers/sayActions");
 
-router.get("/welcome", sayWelcome);
-
-const { browse } = require("../../controllers/programActions");
-
-router.get("/program", browse);
+router.get("/", sayWelcome);
 
 /* ************************************************************************* */
 
 const itemsRouter = require("./items/router");
 
 router.use("/items", itemsRouter);
+
+const programsRouter = require("./programs/router");
+
+router.use("/programs", programsRouter);
 
 /* ************************************************************************* */
 
