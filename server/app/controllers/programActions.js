cf1e3e64b20b1ac98c518d1062aef1fc8924ffbd
Thien Nguyen <thien.ngu25@gmail.com>
2024-05-30 11:53:46 +0200

    quest 06

--- server/app/controllers/programActions.js
+++ server/app/controllers/programActions.js
@@ -1,48 +1,48 @@
-// Import the tables module to use the repositories
+// Some data to make the trick
 const tables = require("../../database/tables");
 
-// Action to browse programs
+const programs = [
+  {
+    id: 1,
+    title: "The Good Place",
+    synopsis:
+      "À sa mort, Eleanor Shellstrop est envoyée au Bon Endroit, un paradis fantaisiste réservé aux individus exceptionnellement bienveillants. Or Eleanor n'est pas exactement une « bonne personne » et comprend vite qu'il y a eu erreur sur la personne. Avec l'aide de Chidi, sa prétendue âme sœur dans l'au-delà, la jeune femme est bien décidée à se redécouvrir.",
+    poster:
+      "https://img.betaseries.com/JwRqyGD3f9KvO_OlfIXHZUA3Ypw=/600x900/smart/https%3A%2F%2Fpictures.betaseries.com%2Ffonds%2Fposter%2F94857341d71c795c69b9e5b23c4bf3e7.jpg",
+    country: "USA",
+    year: 2016,
+  },
+  {
+    id: 2,
+    title: "Dark",
+    synopsis:
+      "Quatre familles affolées par la disparition d'un enfant cherchent des réponses et tombent sur un mystère impliquant trois générations qui finit de les déstabiliser.",
+    poster:
+      "https://img.betaseries.com/zDxfeFudy3HWjxa6J8QIED9iaVw=/600x900/smart/https%3A%2F%2Fpictures.betaseries.com%2Ffonds%2Fposter%2Fc47135385da176a87d0dd9177c5f6a41.jpg",
+    country: "Allemagne",
+    year: 2017,
+  },
+];
+
+// Declare the action
+
 const browse = async (req, res) => {
-  try {
-    // Retrieve programs from the database using the repository
-    const programsFromDB = await tables.program.readAll();
-    let result = programsFromDB;
+  const programsFromDB = await tables.program.readAll();
 
-    // Filter programs if a query parameter is provided
-    if (req.query.q != null) {
-      result = result.filter((program) =>
-        program.synopsis.includes(req.query.q)
-      );
-    }
-
-    res.json(result);
-  } catch (error) {
-    // Handle any errors that occur during the database operation
-    res.status(500).send("Erreur lors de la récupération des programmes.");
-  }
+  res.json(programsFromDB);
 };
 
-// Action to read a specific program by ID
-const read = async (req, res) => {
-  try {
-    const parsedId = parseInt(req.params.id, 10);
+const read = (req, res) => {
+  const parsedId = parseInt(req.params.id, 10);
 
-    // Retrieve all programs from the database
-    const programsFromDB = await tables.program.readAll();
+  const program = programs.find((p) => p.id === parsedId);
 
-    // Find the program with the matching ID
-    const program = programsFromDB.find((p) => p.id === parsedId);
-
-    if (program != null) {
-      res.json(program);
-    } else {
-      res.sendStatus(404);
-    }
-  } catch (error) {
-    // Handle any errors that occur during the database operation
-    res.status(500).send("Erreur lors de la récupération du programme.");
+  if (program != null) {
+    res.json(program);
+  } else {
+    res.sendStatus(404);
   }
 };
+// Export it to import it somewhere else
 
-// Export the actions to use them elsewhere
 module.exports = { browse, read };
